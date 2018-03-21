a56fc3702ebf9f0f8923512c362b046aff1eb38d
Elizabeth Li <sincerelylibby@gmail.com>
2018-03-20 21:00:50 -0700

    Completed underbar part 2.

--- src/underbar.js
+++ src/underbar.js
@@ -182,14 +182,26 @@
     if (accumulator === undefined) {
       output = iterator(collection[0]);
       accumulator = collection[0];
-      for (var i = 1; i < collection.length; i++) {
-        output = iterator(accumulator, collection[i]);
-        accumulator = output;
+      if (Array.isArray(collection)) {
+        for (var i = 1; i < collection.length; i++) {
+          output = iterator(accumulator, collection[i]);
+          accumulator = output;
+        }
+      } else {
+        for (var key in collection) {
+          output = iterator(accumulator, collection[key])
+          accumulator = output;
+        }
       }
-    } else {
+    } else if (Array.isArray(collection)) {
       for (var i = 0; i < collection.length; i++) {
         output = iterator(accumulator, collection[i]);
         accumulator = output; 
+      }
+    } else {
+      for (var key in collection) {
+        output = iterator(accumulator, collection[key])
+        accumulator = output;
       }
     }
     return output; 
@@ -207,16 +219,63 @@
     }, false);
   };
 
-
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    iterator = iterator || _.identity;
+    var truth = [];
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection.length; i++) {
+        if (iterator(collection[i])) {
+          truth.push(true);
+        } else {
+          truth.push(false);
+        }
+      }
+    } else {
+      for (var key in collection) {
+        if (iterator(collection[key])) {
+          truth.push(true);
+        } else {
+          truth.push(false);
+        }
+      }
+    }
+    if (!truth.includes(false)) {
+      return true;
+    } else {
+      return false;
+    }
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    iterator = iterator || _.identity;
+    var truth = [];
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection.length; i++) {
+        if (iterator(collection[i])) {
+          truth.push(true);
+        } else {
+          truth.push(false);
+        }
+      }
+    } else {
+      for (var key in collection) {
+        if (iterator(collection[key])) {
+          truth.push(true);
+        } else {
+          truth.push(false);
+        }
+      }
+    }
+    if (truth.includes(true)) {
+      return true;
+    } else {
+      return false;
+    }
   };
 
 
@@ -239,11 +298,25 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    for (var i = 0; i < arguments.length; i++) {
+      for (var key in arguments[i]) {
+        obj[key] = arguments[i][key];
+      }
+    }
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    for (var i = 0; i < arguments.length; i++) {
+      for (var key in arguments[i]) {
+        if (obj[key] === undefined) {
+          obj[key] = arguments[i][key];
+        }
+      }
+    } 
+    return obj;
   };
 
 
@@ -287,6 +360,14 @@
   // already computed the result for the given argument and return that value
   // instead if possible.
   _.memoize = function(func) {
+    var storage = {};
+    return function() {
+      var args = JSON.stringify(arguments);
+      if (!storage[args]) {
+        storage[args] = func.apply(this, arguments);
+      }
+      return storage[args]; 
+    };
   };
 
   // Delays a function for the given number of milliseconds, and then calls
@@ -296,6 +377,10 @@
   // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
   // call someFunction('a', 'b') after 500ms
   _.delay = function(func, wait) {
+    var args = Array.prototype.slice.call(arguments, 2);
+    setTimeout(function() {
+      func.apply(this, args);
+    }, wait);
   };
 
 
@@ -310,6 +395,15 @@
   // input array. For a tip on how to make a copy of an array, see:
   // http://mdn.io/Array.prototype.slice
   _.shuffle = function(array) {
+    var copy = array.slice(0);
+    var shuffled = [];
+
+    for (var i = 0; i < array.length; i++) {
+      var random = Math.floor(Math.random() * copy.length);
+      shuffled.push(copy[random]);
+      copy.splice(random, 1);
+    }
+    return shuffled;
   };
 
 
